986b453b772c9af7ac595a63163a40984667c9ed
jroebroek <roebroek.jasper@gmail.com>
2024-10-15 12:23:59 +0200

    [test] moving to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
-import os
-
 import numpy
 from Cython.Build import cythonize
 from distutils.extension import Extension
-from setuptools import find_packages, setup
+from setuptools import setup
 
-
-def read_file(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        return file.read()
-
-
-extensions = [Extension("focal_stats.focal_stats.iteration_params",
-                        ["focal_stats/focal_stats/iteration_params.pyx"]),
-              Extension("focal_stats.focal_stats.focal_correlation",
-                        ["focal_stats/focal_stats/focal_correlation.pyx"]),
-              Extension("focal_stats.focal_stats.focal_statistics",
-                        ["focal_stats/focal_stats/focal_statistics.pyx"])
-              ]
+extensions = [
+    Extension(
+        "focal_stats.focal_stats.iteration_params",
+        ["focal_stats/focal_stats/iteration_params.pyx"]),
+    Extension(
+        "focal_stats.focal_stats.focal_correlation",
+        ["focal_stats/focal_stats/focal_correlation.pyx"]),
+    Extension(
+        "focal_stats.focal_stats.focal_statistics",
+        ["focal_stats/focal_stats/focal_statistics.pyx"])
+]
 
 setup(
-    name='focal_stats',
-    version='0.1.0',
-    packages=find_packages(),
-    url='',
-    license='MIT',
-    author='Jasper Roebroek',
-    author_email='roebroek.jasper@gmail.com',
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()],
-    setup_requires=['cython', 'numpy', 'setuptools', 'rasterio'],
-    install_requires=['numpy'],
-    extras_require={
-        'develop': ['cython', 'sphinx', 'sphinx_rtd_theme', 'numpydoc', 'scipy', 'jupyter', 'matplotlib',
-                    'pytest', 'joblib'],
-        'test': ['scipy', 'pytest']},
-    long_description=read_file('README.md'),
-    long_description_content_type='text/markdown',
 )
